6f4f4ebe9c7e3b162a2143e7ead1ffd5f2a0b0d1
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-04 21:35:41 +0900

    説明の記入

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
 
-  def full_title(page_title = "")
+  #ページごとの完全なタイトルを返します
+  def full_title(page_title = "") #引数としてデフォルト値がnilで入っている
     base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
+    if page_title.empty? #引数がnilの時の条件。つまりはデフォルトの返り値
       base_title
     else
       page_title + " | " + base_title
