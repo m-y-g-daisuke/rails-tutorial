7f649c98c94514c4c2b2f7bb6dd06b19b4f6bf6f
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-15 10:49:30 +0900

    email の正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length:{maximum: 50}
   #別の書き方validates (:name,presence:true)
-  validates :email, presence: true, length:{maximum: 255}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  #大文字はrubyでは定数を表す
+  validates :email, presence: true, length:{maximum: 255},
+  format:{with:VALID_EMAIL_REGEX}
 end
