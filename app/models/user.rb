52a57118c17d89dd7c8bf77812605405905cec44
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-15 10:54:13 +0900

    演習問題
    https://rubular.com/を利用
    正規表現の意味を復習すること

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length:{maximum: 50}
   #別の書き方validates (:name,presence:true)
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  #大文字はrubyでは定数を表す
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i  #大文字はrubyでは定数を表す
   validates :email, presence: true, length:{maximum: 255},
   format:{with:VALID_EMAIL_REGEX}
 end
