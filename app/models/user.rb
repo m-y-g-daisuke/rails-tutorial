caac7f07741052a8eef87da1c9c89aa91f303f49
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-16 10:15:05 +0900

    コールバックの設定
    保存前にメールアドレスを小文字にする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save{ self.email = email.downcase}
   validates :name, presence: true, length:{maximum: 50}
   #別の書き方validates (:name,presence:true)
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i  #大文字はrubyでは定数を表す
