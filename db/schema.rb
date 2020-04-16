eacb09887efee13698b97a4f7865cd7f655a65da
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-16 09:48:02 +0900

    indexの追加
    データベースの検索は全表検索となる。
    findクエリを用いて探す可能性のあるカラムにindexを挟むことで、indexが挟まれたカラムを優先的に検索してくれるので、検索の高速化が図れる。

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_001757) do
+ActiveRecord::Schema.define(version: 2020_04_16_004538) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
