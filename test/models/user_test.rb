ac943b4506a1d3d7b6e5916fc2664006bb4bc6b8
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-17 12:18:06 +0900

    破壊的にmailアドレスを保存前に小文字にする

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -57,7 +57,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email addresses shoule be saved as lower-case" do
     mixed_case_email="Foo@ExAMPle.CoM"
-    @user.email = mixed_case_email.downcase
+    @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
