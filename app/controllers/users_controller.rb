cec493f067252e0412c74f55640baaab040d8d12
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-24 11:04:10 +0900

    userのcreateアクションの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
   def new
     @user=User.new
   end
+
+  def create
+    @user=Use.new(params[:user])
+    if @user.save
+      #sucsess
+    else
+      render 'new'
+    end
+  end
+
 end
