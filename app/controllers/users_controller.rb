f561106c6a6e39a952ee75310b577755a51e99b7
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-26 11:39:40 +0900

    ユーザー登録成功時のルーティング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
-      #sucsess
+      redirect_to @user #user_url(@user)と同じ
     else
       render 'new'
     end
