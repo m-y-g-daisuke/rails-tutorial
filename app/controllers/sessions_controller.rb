4356ed0b1474c5941856f871fc8ecc8f1cb75a95
m-y-g-daisuke <disk.myg@gmail.com>
2020-05-04 10:20:16 +0900

    sessinoでのuser認証

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user=Use.find_by(email:params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #action
+    else
+      #show error message
+    render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
