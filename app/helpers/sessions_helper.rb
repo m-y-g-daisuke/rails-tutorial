4f1e45f3c095d641daf6448660e3a1025c2d053e
m-y-g-daisuke <disk.myg@gmail.com>
2020-05-05 15:39:19 +0900

    current_userメソッドの作成
    ||=演算子の利用。
    Userが既にログインしていないならばデータベースから見つけるが、ログイン済みならばインスタンス変数を返している
    データベースのアクセス数を減らせる

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,13 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.user_id
   end
+
+  #現在ログイン中のユーザーを返す(いる場合)
+  def current_user
+    if session[:user_id] # =trueならば
+      @current_user|| = User.find_by(id:session[:user.id])
+      #@current_user = @current_user || User.find_by(id:session[:user.id])
+      # ↑current_userメソッドが呼ばれた時に、左辺のUserを見つけるか、既に入っているならばインスタンス変数を呼び出す
+    end
+  end
 end
