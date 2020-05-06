75d9f35ef0dbcd6305529777a3935db112c8c659
m-y-g-daisuke <disk.myg@gmail.com>
2020-05-06 10:54:50 +0900

    bootstrapにJSの機能が動くように設定

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,13 +2,13 @@ module SessionsHelper
 
   #渡されたユーザーでログインする
   def log_in(user)
-    session[:user_id] = user.user_id
+    session[:user_id] = user.id
   end
 
   #現在ログイン中のユーザーを返す(いる場合)
   def current_user
     if session[:user_id] # =trueならば
-      @current_user|| = User.find_by(id:session[:user.id])
+      @current_user ||= User.find_by(id:session[:user_id])
       #@current_user = @current_user || User.find_by(id:session[:user.id])
       # ↑current_userメソッドが呼ばれた時に、左辺のUserを見つけるか、既に入っているならばインスタンス変数を呼び出す
     end
