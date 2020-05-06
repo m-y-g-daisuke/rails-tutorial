028de1a50060143b87a152459e64c3a78a3eef54
m-y-g-daisuke <disk.myg@gmail.com>
2020-05-06 12:18:42 +0900

    login integration test

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,9 @@ class User < ApplicationRecord
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.creat(string, cost:cost)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
 
 end
