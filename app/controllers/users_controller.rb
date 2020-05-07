0625744462047cb4b9140ed45bb86ff0759e6ac7
m-y-g-daisuke <disk.myg@gmail.com>
2020-05-07 11:08:21 +0900

    userを登録したらログインする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,13 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = " Welcomt to the Sample App!"
       redirect_to @user 
     else
       render 'new'
     end
   end
-
 
 
 private
