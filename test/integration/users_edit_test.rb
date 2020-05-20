84e12662566da914843dfb0a71cced827c957c45
m-y-g-daisuke <disk.myg@gmail.com>
2020-05-20 17:34:45 +0900

    フレンドリーフォワーディングの実装
    レダイレクトした後に、ログインしたら、ログイン前のページに戻してくれる機能。
    sessionの中に入れ込むことで実装できる。
    ７割り理解です。

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -20,10 +20,12 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   end
 
-  test "successful edit" do
-    log_in_as(@user)
+  test "succsessful edit with frindly forwarding" do
     get edit_user_path(@user)
-    assert_template "users/edit"
+    assert_equal session[:forwarding_url], edit_user_url(@user)
+    log_in_as(@user)
+    assert_nil session[:forwarding_url]
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user),params:{user:{name: name,
