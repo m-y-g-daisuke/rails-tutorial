81ae1f1dda9a1cd49ca0e9a3c9017e7225086969
m-y-g-daisuke <disk.myg@gmail.com>
2020-05-23 09:04:31 +0900

    site layout testffの修正

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,11 +2,6 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @user=users(:michael)
-    @other_user=users(:archer)
-  end
-
   test "layout links" do
     get root_path
     assert_template "static_pages/home"
@@ -19,9 +14,29 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title",full_title("Contact")
     get signup_path
     assert_select "title",full_title("Sign up")
+  end
+  
+  
+  def setup
+    @user=users(:michael)
+    @other_user=users(:archer)
+  end
+  
+  test "layout links when logged in user" do
     get users_path
     assert_redirected_to login_path
-    log_in_as @user
+    
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
     get users_path
     assert_select "a[href=?]", user_path(@user)
     assert_select "a[href=?]", user_path(@other_user)
